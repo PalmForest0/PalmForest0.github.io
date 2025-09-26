71e58214c4e189965b13075ecd7258fccdf03c5f
PalmForest <realpalmforest@gmail.com>
2025-09-26 20:03:51 +1000

    Fix Flexboxes

--- src/components/ToolsSection.svelte
+++ src/components/ToolsSection.svelte
@@ -4,17 +4,17 @@
 </script>
 
 {#snippet icon(details: IconProperties)}
-    <Card.Root class="w-40 h-50">
-        <Card.Content class="flex flex-col items-center justify-center gap-4">
-            <img class="mt-0 w-40 h-auto select-none" src={details.imagePath} alt={details.text} />
-            <p class="font-medium text-2xl text-center">{details.text}</p>
+    <Card.Root class="w-34 h-40">
+        <Card.Content class="flex flex-col items-center justify-center gap-2">
+            <img class="mt-[-7px] w-40 h-auto select-none" src={details.imagePath} alt={details.text} />
+            <p class="font-medium text-lg text-center">{details.text}</p>
         </Card.Content>
     </Card.Root>
 {/snippet}
 
 <section class="container px-4 py-16 md:mx-[5vw]">
     <h1 class="text-3xl md:text-4xl font-bold tracking-tight text-balance mb-10">Tools</h1>
-    <div class="flex flex-wrap gap-6">
+    <div class="flex flex-wrap md:justify-start justify-center gap-6">
         {@render icon({ text: ".NET", imagePath: "/icons/dotnet.svg" })}
         {@render icon({ text: "Unity", imagePath: "/icons/unity.svg" })}
         {@render icon({ text: "React", imagePath: "/icons/react.svg" })}
